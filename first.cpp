e2190c8fc63826ca46cb724701c4e19d27f9ed87
sakshipowar1612 <sakshipowar144@gmail.com>
2024-05-21 08:35:13 +0530

    sun in range

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sum_in_range(int x, int y){
+    int result = 0;
+    for (int i = x; i <= y; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int main(){
+    
+    cout << sum_in_range(5, 8) << endl;
+    return 0;
+}
