c8de11c05eb6bad19815db39e3349aec707f7d9e
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-03 08:46:52 +0530

    Diamond Problem

---
+++ OOPS_class2/multipleinh.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class person{
+public:
+    void walk(){
+        cout << "walking... " << endl;
+    }
+
+};
+
+class Teacher : public person{
+public:
+    void teach(){
+        cout << "teaching..." << endl;
+    }
+ 
+};
+
+class researcher : public person{
+
+public:
+    void research(){
+        cout << "researching... " << endl;
+    }
+
+};
+
+
+class professor: public Teacher, public researcher{
+public:
+    void bore()
+    {
+        cout << "boring... " << endl;
+    }
+
+
+};
+
+int main(){
+    professor p;
+    // p.walk(); //request for memeber 'walk' is ambiguous
+    return 0;
+}
