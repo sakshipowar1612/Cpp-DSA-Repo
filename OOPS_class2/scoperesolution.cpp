1e6d230a3bc996a292dd5fa33fa2b56cbd414aac
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-03 09:05:49 +0530

    first solution to a diamond problem: 1.scope resolution

---
+++ OOPS_class2/scoperesolution.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person{
+public:
+    void walk(){
+        cout << "walking... " << endl;
+    }
+
+};
+
+class Teacher : public Person{
+public:
+    void teach(){
+        cout << "teaching..." << endl;
+    }
+ 
+};
+
+class Researcher : public Person{
+
+public:
+    void research(){
+        cout << "researching... " << endl;
+    }
+
+};
+
+
+class Professor: public Teacher, public Researcher{
+public:
+    void bore()
+    {
+        cout << "boring... " << endl;
+    }
+
+
+};
+
+int main(){
+    Professor p;
+    // p.walk(); //request for memeber 'walk' is ambiguous
+    //Diamon Problem
+    //solution 1.Scope Resolution
+    p.Teacher::walk();
+    p.Researcher::walk();
+
+    return 0;
+}
