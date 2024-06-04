9710b32dbc572ff579826c7188cec3b12fedf655
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-04 13:02:13 +0530

    shallowcopy-default copy ctor

---
+++ concepstsOPPS_class2/shallowcopy.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class abc {
+public:
+    int x;
+    int* y;
+    abc(){
+
+    }
+
+    // Constructor
+    abc(int _x, int _y) : x(_x), y(new int(_y)) {
+          // Allocate memory for y and initialize it
+    }
+
+    //default dumb copy constructor: it does shallow copy
+    // abc(const abc &src){
+    //     this->x = src.x;
+    //     this->y = src.y;
+    // }
+
+   
+    // Destructor to free allocated memory
+    ~abc() {
+        delete y;
+    }
+
+    void print() const {
+        printf("x: %d\nPTR y: %p\nContent of y(*y): %d\n\n", x, y, *y);
+    }
+};
+
+int main() {
+    abc obj(10, 20);
+    cout << "printing a: " << endl;
+    obj.print();
+
+    //obj1(obj)
+    abc obj1 = obj; //call copy constructor
+    cout << "printing b: " << endl;
+    obj1.print();
+    
+    *(obj1.y) = 40;
+    cout << "printing b: " << endl;
+    obj1.print();
+    cout << "printing a: " << endl;
+    obj.print();
+
+
+    // abc *a;
+    // abc b = *a;
+    // delete a;
+    // b.print();    
+    return 0;
+}
