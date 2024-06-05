f4340b760e44b969423b4362b86347991ccc3260
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-05 11:15:17 +0530

    downcasting

---
+++ OOPS_class3/downcasting.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using  namespace std;
+
+
+class Parents{
+
+public:
+    void relation(){
+        cout << "I am parent" << endl;
+    }
+
+};
+class Daughter: public Parents{
+
+public:
+    void relation(){
+        cout << "I am daughter" << endl;
+    }
+
+};
+class Son: public Parents{
+
+public:
+    void relation(){
+        cout << "I am parent" << endl;
+    }
+
+};
+
+
+int main(){
+    Parents *p = new Parents();
+    p->relation();
+    Parents *p1 = new Daughter();
+    p1->relation();
+    Daughter *d = new Daughter();
+    d->relation();
+    // Daughter *d = new Parents();  <------------------ giving error (but in lecture sir said that it doesn't give error in c++ but it is a bad practice)
+    // if there is not a virtual function in the class which is at left side, then function at left sides is called.
+
+}
