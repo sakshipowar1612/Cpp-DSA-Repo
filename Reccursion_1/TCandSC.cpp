ba97720a7cabdcdea92ca753fc8c115c2f02e921
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-16 19:18:56 +0530

    Time and Space complexity of reccuesive functions

---
+++ Reccursion_1/TCandSC.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[],int n){
+    if (n==0)
+    {
+        return;
+    }
+    cout << *arr <<" ";
+    printArray(arr+1, n-1);
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printArray(arr, 10);
+}
