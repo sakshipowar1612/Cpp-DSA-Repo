42908a70f6328814cc941dc1a97dc3e34fd88e12
sakshipowar1612 <sakshipowar144@gmail.com>
2024-06-16 20:21:01 +0530

    used Fibonacci series to solve this prob

--- Reccursion_2/leetcode_70.cpp
+++ Reccursion_2/leetcode_70.cpp
@@ -22,17 +22,30 @@
 #include<iostream>
 using namespace std;
 
-int fib(int n){
-    if (n==0 || n==1)
-    {
-        return n;
-    }
-    return fib(n-1)+fib(n-2);
+// int fib(int n){
+//     if (n==0 || n==1)
+//     {
+//         return n;
+//     }
+//     return fib(n-1)+fib(n-2);
     
-}
+// }
+
+// int climbstairs(int n){
+//     return fib(n+1);
+// }
 
 int climbstairs(int n){
-    return fib(n+1);
+    if (n==0)
+    {
+        return 1;
+    }
+    if (n==1)
+    {
+        return 1;
+    }
+    
+    return climbstairs(n-1)+climbstairs(n-2);
 }
 
 int main(){
