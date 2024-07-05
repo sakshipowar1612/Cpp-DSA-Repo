31277a14ecf28b5d2c9469fb7b61c807c1fa8de7
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-05 19:25:23 +0530

    find all the substrings of a given string - INCLUDE EXCLUDE PATTERN

---
+++ Recursion_3/PS_3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void seqOfString(string str, string substr, int index)
+{
+    if (index >= str.length())
+    {
+        cout << "substring = " << substr << endl;
+        return;
+    }
+    else
+    {
+        char ch = str[index];
+        // exclude
+        seqOfString(str, substr, index + 1);
+        // include
+        substr.push_back(ch);
+        seqOfString(str, substr, index + 1);
+
+
+        // //OR
+        // // include
+        // substr.push_back(ch);
+        // seqOfString(str, substr, index + 1);
+        // //exclude
+        // substr.pop_back();
+        // seqOfString(str, substr, index + 1);
+
+        
+    }
+}
+
+void seqOfStringInVec(string str, string substr, int index, vector<string>&vec)
+{
+    if (index >= str.length())
+    {
+        vec.push_back(substr);
+        return;
+    }
+    else
+    {
+        char ch = str[index];
+
+        // // exclude
+        // seqOfStringInVec(str, substr, index + 1, vec);
+        // // include
+        // substr.push_back(ch);
+        // seqOfStringInVec(str, substr, index + 1, vec);
+
+
+        //OR
+        // include
+        substr.push_back(ch);
+        seqOfStringInVec(str, substr, index + 1, vec);
+        //exclude
+        substr.pop_back();
+        seqOfStringInVec(str, substr, index + 1, vec);
+
+        
+    }
+}
+
+int main()
+{
+    seqOfString("abc", "", 0);
+    vector<string>ans;
+    seqOfStringInVec("abc", "", 0, ans);
+    for(string i : ans){
+        cout << i << endl;
+    }
+}
