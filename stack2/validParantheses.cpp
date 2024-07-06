c055469168531bc8d59cdc1f68bae97c516f030a
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-06 19:59:00 +0530

    LeetCode-20 'Valid Parantheses'

---
+++ stack2/validParantheses.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    if (s.length() == 0 || s.length() == 1)
+    {
+        return false;
+    }
+    if (s[0] == ')' || s[0] == '}' || s[0] == ']')
+    {
+        return false;
+    }
+    stack<char> st;
+    int i = 0;
+    while (i != s.length())
+    {
+        char ch = s[i];
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']')
+        {
+            if (ch == ')' && !st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else if (ch == '}' && !st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else if (ch == ']' && !st.empty() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        i++;
+    }
+    if (!st.empty())
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter a string of parentheses: ";
+    cin >> s;
+    if (isValid(s))
+    {
+        cout << "The string is valid." << endl;
+    }
+    else
+    {
+        cout << "The string is not valid." << endl;
+    }
+    return 0;
+}
