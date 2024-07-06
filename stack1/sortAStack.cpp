c462552711e1a0af1f806fcd6a1e80e754383a3c
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-06 17:13:39 +0530

    My solution for sort the stack

---
+++ stack1/sortAStack.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void remove(stack<int>&sta, int data){
+    if (sta.empty())
+    {
+        sta.push(data);
+        return;
+    }
+    int temp = sta.top();
+    if (temp < data)
+    {
+        sta.pop();
+        remove(sta, data);
+        sta.push(temp);
+    }
+    else{
+        sta.push(data);
+        
+    }
+    return;
+}
+
+void sortTheStack(stack<int> &s)
+{
+    if (s.empty())
+    {
+        return;
+    }
+
+    int data = s.top();
+    s.pop();
+    sortTheStack(s);
+    if (s.empty())
+    {
+        s.push(data);
+    }
+    else
+    {
+        if (data > s.top())
+        {
+            remove(s, data);
+            
+        }
+        else{
+            s.push(data);
+        }
+    }
+}
+
+void printStack(stack<int> st)
+{
+    if (st.empty())
+    {
+        return;
+    }
+    int data = st.top();
+    cout << data << " ";
+    st.pop();
+    printStack(st);
+    st.push(data);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(3);
+    st.push(1);
+    st.push(0);
+    st.push(8);
+    st.push(5);
+    st.push(0);
+    sortTheStack(st);
+    printStack(st);
+}
