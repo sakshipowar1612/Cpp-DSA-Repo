ad6a2654167fa9e8376b47b094d09a11897515c6
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-07 20:36:32 +0530

    replace array elemnt by prev smaller element-my approach -  using stack

---
+++ stack3/prevSmallerEle.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+
+void prevSmallerEle(vector<int>&vec){
+    stack<int>s;
+    s.push(-1);
+    for (int i = 0; i < vec.size(); i++)
+    {
+        while (!s.empty() && s.top()>vec[i])
+        {
+            s.pop();
+        }
+        int temp = vec[i];
+        vec[i] = s.top();
+        s.push(temp); 
+    }
+    
+}
+
+void printVec(vector<int> vec)
+{
+    for (int ele : vec)
+    {
+        cout << ele << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    vector<int>vec = {8, 4, 6, 2, 3};
+    printVec(vec);
+    prevSmallerEle(vec);
+    printVec(vec);
+
+}
