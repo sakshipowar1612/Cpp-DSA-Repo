19222eee511c30cd5ca1bbcfff81820c72e58649
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-07 20:01:08 +0530

    replace array elemnt by next smaller element-my approach -  using stack

--- stack3/nextsmallerEle.cpp
+++ stack3/nextsmallerEle.cpp
@@ -3,27 +3,42 @@
 #include <vector>
 using namespace std;
 
-vector<int> nextSmaller(vector<int> &vec)
-{
-    for (int i = 0; i < vec.size(); i++)
+// vector<int> nextSmaller(vector<int> &vec)
+// {
+//     for (int i = 0; i < vec.size(); i++)
+//     {
+//         int min = vec[i];
+//         for (int j = i + 1; j < vec.size(); j++)
+//         {
+//             if (vec[j] < min)
+//             {
+//                 min = vec[j];
+//             }
+//         }
+//         if (min == vec[i])
+//         {
+//             vec[i] = -1;
+//         }
+//         else
+//         {
+//             vec[i] = min;
+//         }
+//     }
+// }
+
+void nextSmallerEle(vector<int>&vec){
+    stack<int>s;
+    s.push(-1);
+    for (int i = vec.size()-1; i >= 0; i--)
     {
-        int min = vec[i];
-        for (int j = i + 1; j < vec.size(); j++)
+        while (!s.empty() && s.top()>vec[i])
         {
-            if (vec[j] < min)
-            {
-                min = vec[j];
-            }
+           s.pop();
         }
-        if (min == vec[i])
-        {
-            vec[i] = -1;
-        }
-        else
-        {
-            vec[i] = min;
-        }
-    }
+        int temp = vec[i];
+        vec[i] = s.top(); 
+        s.push(temp); 
+    }   
 }
 
 void printVec(vector<int> vec)
@@ -39,7 +54,7 @@ int main()
 {
     vector<int> vec = {2, 1, 4, 6, 3};
     printVec(vec);
-    nextSmaller(vec);
+    nextSmallerEle(vec);
     printVec(vec);
 
 }
