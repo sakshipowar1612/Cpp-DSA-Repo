9605bb99c210e0bda048e2e2ed36659cc0bd2a5d
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-09 22:17:09 +0530

    Remove all adjacent duplicates in string - 1047

---
+++ stackAss/removeAllDup.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+string removeDuplicates(string s)
+{
+    stack<char> box;
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (box.empty())
+        {
+            box.push(s[i]);
+            
+        }
+        else if (!box.empty() && s[i] == box.top())
+        {
+            while (!box.empty() && s[i] == box.top())
+            {
+                box.pop();
+            }
+        }
+        else
+        {
+            box.push(s[i]);
+            
+        }
+    }
+    string ans = "";
+    // cout << box.empty() << endl;
+    while (!box.empty())
+    {
+        ans.push_back(box.top());
+        // cout << box.top() << " ";
+        box.pop();
+    }
+    return ans;
+}
+
+int main(){
+    string s = "abbaca";
+    s = removeDuplicates(s);
+    for(char i: s){
+        cout << i << " ";
+    }
+}
