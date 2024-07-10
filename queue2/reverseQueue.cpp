e2ed52d39df2432867572604360467a6690e9769
sakshipowar1612 <sakshipowar144@gmail.com>
2024-07-10 08:31:46 +0530

    Reverse the k elements in the queue using satck

--- queue2/reverseQueue.cpp
+++ queue2/reverseQueue.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 //U can also solve using stack;
@@ -14,6 +15,8 @@ void reverseQueue(queue<int>&q){
     q.push(data);  
 }
 
+
+// <------------------My Approach--------------------->
 void reverseKEle(queue<int>&q, int k){
     queue<int>ans;
     int size = q.size();
@@ -28,10 +31,30 @@ void reverseKEle(queue<int>&q, int k){
     {
         ans.push(q.front());
         q.pop();
-    }
-    
+    } 
     q = ans;
+}
 
+
+void reverseKEleOfQ(queue<int>&q, int k){
+    stack<int>s;
+    int n = q.size()-k;
+    for (int i = 0; i < k; i++)
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while (!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    while (n)
+    {
+        q.push(q.front());
+        q.pop();
+        n--;
+    }   
 }
 
 void printQ(queue<int>q){
@@ -55,7 +78,8 @@ int main(){
     q.push(50);
     printQ(q);
     // reverseQueue(q);
-    reverseKEle(q, 3);
+    // reverseKEle(q, 3);
+    reverseKEleOfQ(q,3);
     printQ(q);
 
 
